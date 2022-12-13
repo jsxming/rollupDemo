816dc0f4cf375c13e42478c20197ebe9deacd845
田定明 <923727421@qq.com>
2022-12-13 10:36:43 +0800

    feat: 添加babel功能转换新语法

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from '@rollup/plugin-typescript';
 // 目前，大多数的 NPM 包暴露的都是 CommonJS 模块。在此更改之前，我们需要将 CommonJS 转换为 ES2015，这样 Rollup 才能处理它们
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import babel from '@rollup/plugin-babel';
 const formatArr = ['amd', 'cjs', 'esm', 'iife', 'umd']
 
 export default {
@@ -15,7 +16,14 @@ export default {
       name: 'file'
     }
   }),
-  plugins: [resolve(), json(), commonjs(), typescript(), strip()],
+  plugins: [
+    resolve(),
+    babel({ babelHelpers: 'bundled' }),
+    json(),
+    commonjs(),
+    typescript(),
+    strip()
+  ],
   // 指出哪些模块需要被视为外部引入
   //  external: ['lodash']
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import foo from './foo';
 import json from '../package.json';
 
 export default function () {
+  const test = ()=>{
+    console.log('test')
+  }
+  test()
   // import('./import').then((r) => {
   //   console.log(r.default)
   // }).catch((err) => {
